4ec8078934c58fa204e50b995e6dfc9dafe4b4e2
Archit <jainarchit1604@gmail.com>
2023-11-01 18:59:09 +0530

    Changed input file

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ var loadFile = function(event) {
     var input = event.target;
     var file = input.files[0];
     var type = file.type;
+    var getter=document.getElementById('dummy');
+    getter.classList.add("hidden");
 
     var output = document.getElementById('preview_img');
-
+    output.classList.remove("hidden");
 
     output.src = URL.createObjectURL(event.target.files[0]);
     output.onload = function() {
